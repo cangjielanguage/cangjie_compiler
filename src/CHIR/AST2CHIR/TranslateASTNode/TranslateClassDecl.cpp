164ee56b9f906a7e4e700a49cedc9416b212193b
duo-duo <duanwenxue@huawei.com>
2025-11-12 10:02:49 +0000

    fix: cjmp cjcov
    Signed-off-by: duo-duo <duanwenxue@huawei.com>

--- src/CHIR/AST2CHIR/TranslateASTNode/TranslateClassDecl.cpp
+++ src/CHIR/AST2CHIR/TranslateASTNode/TranslateClassDecl.cpp
@@ -153,8 +153,7 @@ Func* Translator::ClearOrCreateVarInitFunc(const AST::Decl& decl)
         CJC_ASSERT(params.size() == 1);
         func->ReplaceBody(*body);
         func->AddParam(*params[0]);
-        const auto& loc = DebugLocation(TranslateLocationWithoutScope(builder.GetChirContext(), decl.begin, decl.end));
-        func->SetDebugLocation(loc);
+        func->SetDebugLocation(DebugLocation());
     } else {
         auto identifier = decl.identifier + POSTFIX;
         auto rawMangledName = decl.rawMangleName + POSTFIX;
